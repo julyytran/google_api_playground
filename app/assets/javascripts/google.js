ce17e4dd18b35cb01c68723e202e5244fb66cd12
July Tran <julyytran@gmail.com>
2016-04-11 17:30:01 -0600

    no working api calls Faraday returns SSL error or no body when block is passed Ajax call error says localhost is not allowed Postman says invalid request

---
+++ app/assets/javascripts/google.js
@@ -0,0 +1,3 @@
+$.getJSON("https://maps.googleapis.com/maps/api/distancematrix/json?origin=1510+Blake+Street+Denver+Colorado&destination=6715+West+Colfax+Avenue+Lakewood+Colorado")
+// removed hard-coded key from end of uri 
+// debugger
--- app/services/google_service.rb
+++ app/services/google_service.rb
@@ -2,13 +2,14 @@ class GoogleService
   attr_reader :connection, :params
 
    def initialize
-      # @params = "origin=1510+Blake+Street+Denver+Colorado&destination=6715+West+Colfax+Avenue+Lakewood+Colorado&key=#{ENV['GOOGLE_MAPS_DISTANCE_MATRIX_KEY']}"
-      @connection = Faraday.new("https://maps.googleapis.com/maps/api/distancematrix/json?") do |faraday|
+      @params = "origin=1510+Blake+Street+Denver+Colorado&destination=6715+West+Colfax+Avenue+Lakewood+Colorado&key=#{ENV['GOOGLE_MAPS_DISTANCE_MATRIX_KEY']}"
+      @connection = Faraday.new("https://maps.googleapis.com/maps/api/distancematrix/json?")
+      # do |faraday|
         # faraday.adapter  Faraday.default_adapter
-        faraday.params[:key] = ENV['GOOGLE_MAPS_DISTANCE_MATRIX_KEY']
-        faraday.params[:origin] = "1510+Blake+Street+Denver+Colorado"
-        faraday.params[:destination] = "6715+West+Colfax+Avenue+Lakewood+Colorado"
-      end
+        # faraday.params[:origin] = "1510+Blake+Street+Denver+Colorado"
+        # faraday.params[:destination] = "6715+West+Colfax+Avenue+Lakewood+Colorado"
+        # faraday.params[:key] = ENV['GOOGLE_MAPS_DISTANCE_MATRIX_KEY']
+      # end
    end
 
    def get
